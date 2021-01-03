e9ae604e3dac57f2da5cb2c92b0f6ffb29689d08
Alon Rubin <alonrubin2@gmail.com>
2021-01-03 02:26:41 +0200

    gradient to-right just looks better

--- src/GradiantButton/GradiantButton.js
+++ src/GradiantButton/GradiantButton.js
@@ -12,7 +12,7 @@ class GradiantButton extends Component {
     render() {
         return (
             <div className="gradiant-box" color1={this.props.color1} color2={this.props.color2} height={this.props.height} 
-            style={{background: `linear-gradient(to bottom right, ${this.props.color1}, ${this.props.color2}`, height: this.props.height+'px', width: this.props.height*3.2+'px', display: "inline-block", boxShadow: '10px 10px #888888'}}>
+            style={{background: `linear-gradient(to right, ${this.props.color1}, ${this.props.color2}`, height: this.props.height+'px', width: this.props.height*3.2+'px', display: "inline-block", boxShadow: '10px 10px #888888'}}>
                 
             </div>
         );
